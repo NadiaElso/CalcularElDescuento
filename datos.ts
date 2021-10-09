406017a8923215bd2a1a16fb47081b364899025c
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2021-10-09 13:06:31 -0300

    agreguè la variable precio final

--- datos.ts
+++ datos.ts
@@ -1,12 +1,15 @@
 let btnEnv = document.getElementById("btnEnviar");
-let PrecioInicial= document.getElementById("PrecioInicial");
+let PrecioInicial = document.getElementById("PrecioInicial");
 let Descuento = document.getElementById("Descuento");
+let PrecioFinal = document.getElementById("PrecioFinal");
 
 btnEnv.addEventListener("click", () => {
   // TU CODIGO VA AQUI, POR EJEMPLO:
   let numeroA: number = Number(PrecioInicial.value);
   let numeroB: number = Number(Descuento.value);
-  let resultado: number = (numeroA * numeroB)/100;
+  let numeroC: number = Number(PrecioFinal.value);
+  let resultado: number = (numeroA * numeroB) / 100;
+  let resultadoTotal: number = PrecioInicial - resultado;
   console.log("el descuento es de:");
   console.log(resultado);
   //}
